85eb0f14d11b22d9a57f28065e2d00d6d4f93377
Palak Khatri <138775553+palakkhatri18@users.noreply.github.com>
2024-07-13 23:53:44 +0530

    Add files via upload

---
+++ Binary Array Sorting.py
@@ -0,0 +1,9 @@
+class Solution:
+    
+    #Function to sort the binary array.
+    def binSort(self, A, N): 
+        #Your code here
+        #No need to print the array
+        A.sort()
+        return A
+    
